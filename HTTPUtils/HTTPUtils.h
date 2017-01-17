5221db2f3e5fc5bb0e6eca5e91dde571f40e0e35
Stuart Pineo <svpineo@gmail.com>
2017-01-17 09:22:56 -0500

    Added the core functionality to access the Reachablity libs (refs #E256)

---
+++ HTTPUtils/HTTPUtils.h
@@ -0,0 +1,16 @@
+//
+//  HTTPUtils.h
+//  AcrylicsColorPicker
+//
+//  Created by Stuart Pineo on 12/8/16.
+//  Copyright © 2016 Stuart Pineo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HTTPUtils : NSObject
+
++ (BOOL)networkIsReachable;
+//+ (BOOL)HTTPGet:(NSString *)urlStr contentType:(NSString *)contentType fileName:(NSString *)fileName;
+
+@end
